5af370a8c7ccdb19eeacd1e174b4078677365e2b
Paolo Sciarra <itbhps@gmail.com>
2021-10-03 12:55:20 +0200

    added README

---
+++ README.md
@@ -0,0 +1,5 @@
+In order to be an acceptable password, a string must:
+
+- have a length grater than 7 characters
+- contain at least one alphabetic character
+- contain at least one digit
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,11 +7,7 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
-
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
 }
 
 repositories {
@@ -22,19 +18,8 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:29.0-jre")
-
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
-
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api("org.apache.commons:commons-math3:3.6.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.1")
+    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.12.4")
 }
