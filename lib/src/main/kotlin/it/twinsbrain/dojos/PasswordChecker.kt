7eefa75e06544e0168629e7921c0311f01f436d0
Paolo Sciarra <itbhps@gmail.com>
2021-10-04 08:17:28 +0200

    extracted constraint concept

--- lib/src/main/kotlin/it/twinsbrain/dojos/PasswordChecker.kt
+++ lib/src/main/kotlin/it/twinsbrain/dojos/PasswordChecker.kt
@@ -1,8 +1,8 @@
 package it.twinsbrain.dojos
 
-class PasswordChecker {
+class PasswordChecker(private val constraint: Constraint) {
   fun verify(password: String): Boolean {
-    return LengthConstraint.apply(password)
+    return constraint.apply(password)
   }
 }
 
@@ -10,6 +10,17 @@ interface Constraint {
   fun apply(password: String): Boolean
 }
 
-object LengthConstraint : Constraint{
+class ComposedConstraint(private vararg val constraint: Constraint) : Constraint {
+  override fun apply(password: String): Boolean = constraint.all { it.apply(password) }
+}
+
+object LengthConstraint : Constraint {
   override fun apply(password: String): Boolean = password.length >= 7
+}
+
+object AlphaNumericConstraint : Constraint {
+  override fun apply(password: String): Boolean = password.isAlphaNumeric()
+
+  private fun String.isAlphaNumeric(): Boolean = this.filter { it in 'A'..'Z' || it in 'a'..'z' || it in '0'..'9' }
+      .length == this.length
 }
--- lib/src/test/kotlin/it/twinsbrain/dojos/PasswordCheckerTest.kt
+++ lib/src/test/kotlin/it/twinsbrain/dojos/PasswordCheckerTest.kt
@@ -6,19 +6,20 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 internal class PasswordCheckerTest {
-    private val underTest = PasswordChecker()
 
-    @Test
-    fun `it should have at least seven chars`() {
-        val failMessage = "password should have minimum 7 chars"
-        assertFalse(underTest.verify("123"), failMessage)
-        assertTrue(underTest.verify("1234567"), failMessage)
-        assertTrue(underTest.verify("123456789"), failMessage)
-    }
+  @Test
+  fun `it should have at least seven chars`() {
+    val underTest = PasswordChecker(LengthConstraint)
+    val failMessage = "password should have minimum 7 chars"
+    assertFalse(underTest.verify("123"), failMessage)
+    assertTrue(underTest.verify("1234567"), failMessage)
+    assertTrue(underTest.verify("123456789"), failMessage)
+  }
 
-    @Disabled
-    fun `it should contain just letters and digits`() {
-        val failMessage = "password should have be alphanumeric"
-        assertFalse(underTest.verify(",!()>?/"), failMessage)
-    }
+  @Test
+  fun `it should contain just letters and digits`() {
+    val underTest = PasswordChecker(AlphaNumericConstraint)
+    val failMessage = "password should have be alphanumeric"
+    assertFalse(underTest.verify(",!()>?/"), failMessage)
+  }
 }
